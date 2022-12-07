b313ecc1920c2a05f690fa01d819a13439d9a0ed
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-12-07 08:26:27 +0700

    re layouting

--- src/pages/MenteList.jsx
+++ src/pages/MenteList.jsx
@@ -10,11 +10,11 @@ import { Link } from "react-router-dom";
 function MenteList() {
   return (
     <Layout>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-6">
         <div>
           <Sidebar menteeFontStile={"font-bold"} menteeShadow={"shadow-lg"} />
         </div>
-        <div className="col-span-3 bg-abuAltera rounded-r-3xl h-screen overflow-auto">
+        <div className="col-span-5 bg-abuAltera rounded-r-3xl h-screen overflow-auto">
           <Navbar title={"Mentee List"} name={"John Doe"} />
           <div className="p-5">
             <div className="bg-white rounded-lg shadow-lg">
